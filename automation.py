fa5541077a1326d40e7f6e7752a705f2667aa2d8
Tamir Lousky <tlousky@gmail.com>
2014-02-01 17:17:45 +0200

    trying to define props as percentages

--- automation.py
+++ automation.py
@@ -396,6 +396,7 @@ class insole_props( bpy.types.PropertyGroup ):
     flat_area = bpy.props.FloatProperty(
         description = "Percentage of scan to be flattened, from front to back",
         name        = "Flat Area",
+        subtype     = {'PERCENTAGE'},
         default     = 0.25,
         min         = 0.0,
         max         = 1.0,
@@ -405,6 +406,7 @@ class insole_props( bpy.types.PropertyGroup ):
     falloff = bpy.props.FloatProperty(
         name        = "Lamp light intensity",
         description = "Falloff area of flattenning tool",
+        subtype     = {'PERCENTAGE'},
         default     = 0.25,
         min         = 0.0,
         max         = 1.0,
