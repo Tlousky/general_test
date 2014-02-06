d28048cdf49cd44376ea42e76967962c70035258
Tamir Lousky <tlousky@gmail.com>
2014-02-06 19:16:04 +0200

    boolean operation still faulty. need to find solution, but this is a reasonable intermediate solution

--- automation.py
+++ automation.py
@@ -698,11 +698,10 @@ class create_insole_from_curve( bpy.types.Operator ):
             TRANSFORM_OT_translate={"value":(0, 0, extrude_z )} 
         )
 
-        # Go to object mode
-        bpy.ops.object.mode_set( mode = 'OBJECT' )
-        
         # Recalculate normals on both objects
         for obj in c, scan:
+            bpy.ops.object.mode_set( mode = 'OBJECT' )
+            
             bpy.ops.object.select_all( action = 'DESELECT' )
             obj.select = True        # Select obj
             scn.objects.active = obj # Set scan as active object
@@ -723,18 +722,28 @@ class create_insole_from_curve( bpy.types.Operator ):
         scn.objects.active = scan # Set scan as active object
         
         m = scan.modifiers.new( 'insole_boolean', 'BOOLEAN' )
-        m.operation = 'DIFFERENCE'
+        m.operation = 'INTERSECT'
         m.object    = c
 
+        # Create boolean modifier on curve
+        bpy.ops.object.select_all( action = 'DESELECT' )
+        c.select = True        # Select scan
+        scn.objects.active = c # Set scan as active object
+        
+        m = c.modifiers.new( 'insole_boolean', 'BOOLEAN' )
+        m.operation = 'DIFFERENCE'
+        m.object    = scan
+
         # Apply boolean modifier and delete (or hide) other object
-        bpy.ops.object.modifier_apply( modifier = 'insole_boolean' )
+        # bpy.ops.object.modifier_apply( modifier = 'insole_boolean' )
         
         # Delete now useless converted curve
+        '''
         bpy.ops.object.select_all( action = 'DESELECT' )
         c.select = True        # Select curve
         scn.objects.active = c # Set curve as active object
         bpy.ops.object.delete()
-        
+        '''
         return {'FINISHED'}
         
 class insole_props( bpy.types.PropertyGroup ):
